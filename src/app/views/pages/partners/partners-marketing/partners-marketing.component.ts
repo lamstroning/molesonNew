1a7fd6166cfa715fc630c87539ce8e25f657845d
Kondin Dmitriy <dkondin@gmail.com>
2020-08-07 12:39:52 +0700

    верстка маркетинг-плана

--- src/app/views/pages/partners/partners-marketing/partners-marketing.component.ts
+++ src/app/views/pages/partners/partners-marketing/partners-marketing.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-partners-marketing',
   templateUrl: './partners-marketing.component.html',
-  styleUrls: ['./partners-marketing.component.scss']
+  styleUrls: ['../partners-analytics/partners-analytics.component.scss']
 })
 export class PartnersMarketingComponent implements OnInit {
 
